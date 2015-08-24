03c7a7e425066a5efbd97675a3338ac29c7bd268
Andrew Brennwald & Alexa Zeazas <student@epicodus.com>
2015-08-24 12:25:55 -0700

    Add basic structure for Division and Employee classes.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
+require("rspec")
+require("pry")
+require("capybara")
+require("sinatra/activerecord")
+require("division")
+require("employee")
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Division.all().each() do |division|
+      division.destroy()
+    end
+    Employee.all().each() do |employee|
+      employee.destroy()
+    end
+  end
+end
